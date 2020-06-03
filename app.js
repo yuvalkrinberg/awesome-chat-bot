30dc27a98dfeb0d3a4599debaa58436c6c19f508
Yuval Krinberg <35220490+yuvalkrinberg@users.noreply.github.com>
2020-06-03 16:41:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 server = app.listen(port, () => {
   console.log('server is running on port 3000');
